8eced64f4ecf77bb9e965f7478014036bdd3f941
Brindha Ramesh <brindhu@MacBook-Pro.local>
2020-02-19 10:11:31 -0800

    devserver

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 
 class App extends React.Component {
     state = {
-      count: 0,
+      count: 1,
     };
   
     increment = () => {
@@ -11,20 +11,16 @@ class App extends React.Component {
   
       this.setState({
         // eslint-disable-next-line no-magic-numbers
-        count: currentCount + 1,
+        count: currentCount * 2,
       });
     };
-
-      state = {
-        count: 0,
-      };
 
     decrement = () => {
       const currentCount = this.state.count;
   
       this.setState({
         // eslint-disable-next-line no-magic-numbers
-        count: currentCount - 1,
+        count: currentCount / 2,
       });
     };
   
